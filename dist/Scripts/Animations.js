e8b1ba1bd5a6cb99ed1296493aa12f006adde062
Taylor Michel <tmichel@tmichelpro.com>
2020-03-31 13:27:42 -0400

    Removed dist folder

--- dist/Scripts/Animations.js
+++
@@ -1,9 +0,0 @@
-import React from './node_modules/react';
-const Animations = {
-  Fade: 'Fade',
-  SlideFromLeft: 'SlideFromLeft',
-  SlideFromRight: 'SlideFromRight',
-  SlideFromTop: 'SlideFromTop',
-  SlideFromBottom: 'SlideFromBottom'
-};
-export default Animations;
